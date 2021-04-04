441474cdcb04b459879a6f3a4e456a5bbf7bcb71
Aleksandr Bohan <Alex_Boh@epam.com>
2021-04-04 18:40:37 +0300

    I have done two tasks.

--- 2_Algoritmization/src/OneDimensionalArrays.java
+++ 2_Algoritmization/src/OneDimensionalArrays.java
@@ -1,21 +1,45 @@
 public class OneDimensionalArrays {
     private int sumOfMultiples;
+
+private void replacingElementsInAnArray(int minimumNumberInSequence, int maximumNumberInSequence, int numberZ){
+int [] arrayOfNumbersInSequence = new int[maximumNumberInSequence-minimumNumberInSequence];
+int j = 0;
+int counter = 0;
+for (int i = minimumNumberInSequence; i<maximumNumberInSequence-1; i++){
+       arrayOfNumbersInSequence[j] = i;
+    if (arrayOfNumbersInSequence[j]>numberZ){
+        arrayOfNumbersInSequence[j] = numberZ;
+        counter++;
+    }
+    System.out.println("Element of array: " + arrayOfNumbersInSequence[j]);
+    j++;
+}
+    System.out.println("Count of elements = numberZ are  " + counter);
+}
+
     private void multiples(int numberK, int minNumberInArray, int maxNumberInArray){
         int[] A = new int[maxNumberInArray-minNumberInArray];
+        int j = 0;
         for (int i = minNumberInArray; i<maxNumberInArray-1; i++){
-            A[i] = i;
-            if (numberK % A[i] == 0){
-                sumOfMultiples += i;
+
+            A[j] = i;
+
+            if (numberK % A[j] == 0 & A[j]!=0){
+                sumOfMultiples += A[j];
                 System.out.println("Multiple number: " + i);
                 System.out.println("Sum of multiples =  " + sumOfMultiples);
             }
+            j++;
         }
 
     }
 
+
+
     public static void main(String[] args) {
         OneDimensionalArrays oneDimensionalArrays = new OneDimensionalArrays();
         oneDimensionalArrays.multiples(45, 1, 20);
+        oneDimensionalArrays.replacingElementsInAnArray(10, 34, 19);
     }
 
 }
