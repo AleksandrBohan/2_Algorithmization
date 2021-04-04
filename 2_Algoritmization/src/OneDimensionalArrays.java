7c882c81eaa29ad1703f8ccdb7fd4fe8bc1d80a0
Aleksandr Bohan <Alex_Boh@epam.com>
2021-04-04 17:57:29 +0300

    I have done first task from "one-dimension arrays" part.

--- 2_Algoritmization/src/OneDimensionalArrays.java
+++ 2_Algoritmization/src/OneDimensionalArrays.java
@@ -1,3 +1,20 @@
 public class OneDimensionalArrays {
+    private int sumOfMultiples;
+    private void multiples(int numberK, int minNumberInArray, int maxNumberInArray){
+        int[] A = new int[maxNumberInArray-minNumberInArray];
+        for (int i = minNumberInArray; i<maxNumberInArray; i++){
+            if (numberK % i == 0){
+                sumOfMultiples += i;
+                System.out.println("Multiple number: " + i);
+                System.out.println("Sum of multiples =  " + sumOfMultiples);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        OneDimensionalArrays oneDimensionalArrays = new OneDimensionalArrays();
+        oneDimensionalArrays.multiples(45, 1, 20);
+    }
 
 }
