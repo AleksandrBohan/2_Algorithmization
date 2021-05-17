93a58b947e7535e2ab571f4eb76c2b0b79d4766b
Aleksandr Bohan <Alex_Boh@epam.com>
2021-05-17 13:56:58 +0300

    I haven't ended 6th task of this part yet, but ended 7th.

--- 2_Algoritmization/src/ArraysOfArray.java
+++ 2_Algoritmization/src/ArraysOfArray.java
@@ -4,7 +4,52 @@ public class ArraysOfArray {
 
    private Random random = new Random(100);
 
+   private void task7(int n){
+       int count = 0;
+       int [][] squareMatrix = new int[n][n];
+       System.out.println();
+       for (int i = 0; i<n; i++){
+           System.out.println();
+           for (int j = 0; j<n; j++){
+               squareMatrix[i][j] = (int) Math.sin((Math.pow(i,2)- Math.pow(j, 2))/n);
+               if (squareMatrix[i][j] >= 0){
+                   count++;
+               }
+               System.out.print(squareMatrix[i][j] + "  ");
+           }}
+       System.out.println("Count of pozitive numbers = " + count);
+   }
+
+   private void task6(int n){
+       if (n%2 == 0){
+           int count = 0;
+
+           int [][] squareMatrix = new int[n][n];
+           System.out.println();
+           for (int i = 0; i<n; i++){
+               count++;
+               System.out.println();
+               for (int j = count-1; j<n-count+1; j++){
+                   squareMatrix[i][j] = 1;
+                   System.out.print(squareMatrix[i][j] + "  ");
+
+               }
+               int countForWhile = 0;
+               while (countForWhile<count-1){
+                   squareMatrix[i][countForWhile] = 0;
+                   System.out.print(squareMatrix[i][countForWhile] + "  ");
+                   squareMatrix[i][n-countForWhile-1] = 0;
+                   System.out.print(squareMatrix[i][n-countForWhile-1] + "  ");
+                   countForWhile++;
+               }
+           }
+       }
+       else System.out.println("Enter a even number!");
+       //TODO
+   }
+
    private void task5(int n){
+       if (n%2 == 0){
        int count = n+1;
 
        int [][] squareMatrix = new int[n][n];
@@ -25,25 +70,32 @@ public class ArraysOfArray {
            }
        }
    }
+       else System.out.println("Enter a even number!");
 
-   private void task4(int sizeOfMatrix){
+   }
 
-       int [][] squareMatrix = new int[sizeOfMatrix][sizeOfMatrix];
-       for (int i = 0; i<sizeOfMatrix; i++){
-           System.out.println();
-           int countForColumn = 1;
-           int countForStrings = sizeOfMatrix;
-           for (int j = 0; j<sizeOfMatrix; j++){
-               if (i % 2 == 0){
-               squareMatrix[i][j] = countForColumn;
-               System.out.print(squareMatrix[i][j] + "  ");
-               countForColumn++;
+   private void task4(int sizeOfMatrix) {
+       if (sizeOfMatrix % 2 == 0) {
+           int[][] squareMatrix = new int[sizeOfMatrix][sizeOfMatrix];
+           for (int i = 0; i < sizeOfMatrix; i++) {
+               System.out.println();
+               int countForColumn = 1;
+               int countForStrings = sizeOfMatrix;
+               for (int j = 0; j < sizeOfMatrix; j++) {
+                   if (i % 2 == 0) {
+                       squareMatrix[i][j] = countForColumn;
+                       System.out.print(squareMatrix[i][j] + "  ");
+                       countForColumn++;
+                   } else {
+                       squareMatrix[i][j] = countForStrings;
+                       System.out.print(squareMatrix[i][j] + "  ");
+                       countForStrings--;
+                   }
+               }
            }
-               else {squareMatrix[i][j] = countForStrings;
-               System.out.print(squareMatrix[i][j] + "  ");
-               countForStrings--;
-           }
-   }}}
+       }
+else System.out.println("Enter a even number!");
+   }
 
    private void task3(int sizeOfMatrix, int k, int p){
        int [][] squareMatrix = new int[sizeOfMatrix][sizeOfMatrix];
@@ -116,6 +168,8 @@ int count = n-1;
         arraysOfArray.task3(4, 3, 2);
         arraysOfArray.task4(4);
         arraysOfArray.task5(6);
+        arraysOfArray.task6(4);
+        arraysOfArray.task7(9);
     }
 
 }
