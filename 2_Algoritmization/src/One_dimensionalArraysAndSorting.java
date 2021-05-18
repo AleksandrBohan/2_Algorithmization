63ee6135e39d085ac981b59f78f905d3bec0d68a
Aleksandr Bohan <Alex_Boh@epam.com>
2021-05-18 08:31:05 +0300

    I haven't done task2 from this part.

--- 2_Algoritmization/src/One_dimensionalArraysAndSorting.java
+++ 2_Algoritmization/src/One_dimensionalArraysAndSorting.java
@@ -2,6 +2,38 @@ import java.util.Random;
 
 public class One_dimensionalArraysAndSorting {
     private Random random = new Random();
+
+    private void task2(int sizeOfFirstArr, int sizeOfSecondArr,
+                       int firstNumberOfFirstArr, int firstNumberOfSecondArr) {
+
+        int[] firstArray = new int[sizeOfFirstArr + sizeOfSecondArr];
+        int[] secondArray = new int[sizeOfSecondArr];
+
+        for (int i = 0; i < sizeOfFirstArr; i++) {
+            firstArray[i] = firstNumberOfFirstArr;
+            firstNumberOfFirstArr++;
+        }
+
+        for (int j = 0; j < sizeOfSecondArr; j++) {
+            secondArray[j] = firstNumberOfSecondArr;
+            firstNumberOfSecondArr++;
+        }
+        System.out.println();
+        for (int i = 0; i < sizeOfSecondArr; i++) {
+
+            for (int j = 0; j < sizeOfFirstArr; j++) {
+                if (firstArray[i] >= secondArray[j]) {
+                    for (int k = i; k < sizeOfFirstArr + sizeOfSecondArr; k++) {
+                        firstArray[k] = secondArray[j];
+                        System.out.print(firstArray[k] + "  ");
+                    }
+                } else System.out.print(firstArray[i] + "  ");
+
+
+            }
+        }
+    }
+
     private void task1(int sizeOfFirstArr, int sizeOfSecondArr, int naturalNumberK){
 
         int count = 0;
@@ -35,5 +67,6 @@ public class One_dimensionalArraysAndSorting {
     public static void main(String[] args) {
         One_dimensionalArraysAndSorting one_dimensionalArraysAndSorting = new One_dimensionalArraysAndSorting();
         one_dimensionalArraysAndSorting.task1(3,4,2);
+        one_dimensionalArraysAndSorting.task2(3,4,4,2);
     }
 }
