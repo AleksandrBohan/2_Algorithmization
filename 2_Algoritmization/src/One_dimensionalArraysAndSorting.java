5655a54ae09765e59dafeb980911a823f8cb3e9a
Aleksandr Bohan <Alex_Boh@epam.com>
2021-05-18 09:30:17 +0300

    I haven't done task5 because I didn't understand requirements.

--- 2_Algoritmization/src/One_dimensionalArraysAndSorting.java
+++ 2_Algoritmization/src/One_dimensionalArraysAndSorting.java
@@ -3,6 +3,40 @@ import java.util.Random;
 public class One_dimensionalArraysAndSorting {
     private Random random = new Random();
 
+    public int binarySearch(int arr[], int firstElement, int lastElement, int elementToSearch){
+        // условие прекращения
+        if (lastElement >= firstElement) {
+            int mid = firstElement + (lastElement - firstElement) / 2;
+
+            // если средний элемент - целевой элемент, вернуть его индекс
+            if (arr[mid] == elementToSearch)
+                return mid;
+
+            // если средний элемент больше целевого
+            // вызываем метод рекурсивно по суженным данным
+            if (arr[mid] > elementToSearch)
+                return binarySearch(arr, firstElement, mid - 1, elementToSearch);
+
+            // также, вызываем метод рекурсивно по суженным данным
+            return binarySearch(arr, mid + 1, lastElement, elementToSearch);
+        }
+
+        return -1;
+    }
+
+
+    private void task5(int sizeOfArray){
+        System.out.println();
+        int [] arrayForSorting = new int[sizeOfArray];
+        int valueInStack = 0;
+        System.out.println();
+        for (int i = 0; i<sizeOfArray; i++){
+            arrayForSorting[i] =random.nextInt(100);
+            System.out.print(arrayForSorting[i] + "  ");
+        }
+//TODO не понял условие!!
+    }
+
     private void task4(int sizeOfArray){
         System.out.println();
         int [] arrayForSorting = new int[sizeOfArray];
