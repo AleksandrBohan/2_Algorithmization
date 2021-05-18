8125e5656447c25d6d4b6220f4fc9543b57abc46
Aleksandr Bohan <Alex_Boh@epam.com>
2021-05-18 13:31:26 +0300

    I haven't done task2 from this part.

--- 2_Algoritmization/src/DecompositionWithMethods.java
+++ 2_Algoritmization/src/DecompositionWithMethods.java
@@ -1,6 +1,17 @@
 
 public class DecompositionWithMethods {
 
+    private void task2(int a, int b, int c, int d){
+        int divNumber = 1;
+        while (a % divNumber != 0 & b % divNumber !=0
+        & c % divNumber != 0 & d % divNumber != 0 | divNumber == 1){
+                       divNumber++;
+        }
+        System.out.println("NOD for these numbers is " + divNumber);
+
+        //TODO
+    }
+
 
     private void task1(int a, int b){
 if (NOK(a, b) == (a*b)/NOD(a,b)){
@@ -12,8 +23,8 @@ if (NOK(a, b) == (a*b)/NOD(a,b)){
  private int NOD(int a, int b){
      int c = 1;
 
-     while (a%c == 0 & b%c ==0){
-         c++;
+     while (a%c != 0 & b%c !=0){
+            c++;
      }
      System.out.println("NOD of numbers a and b = " + c);
      return c;
@@ -23,6 +34,9 @@ if (NOK(a, b) == (a*b)/NOD(a,b)){
      int c = 1;
 
      while (c%a != 0 & c%b != 0){
+         if (a == 1 | b == 1){
+             break;
+         }
          c++;
      }
      System.out.println("NOK of numbers a and b = " + c);
@@ -32,5 +46,6 @@ if (NOK(a, b) == (a*b)/NOD(a,b)){
     public static void main(String[] args) {
         DecompositionWithMethods decompositionWithMethods = new DecompositionWithMethods();
         decompositionWithMethods.task1(5,7);
+        decompositionWithMethods.task2(7,14,28,42);
     }
 }
